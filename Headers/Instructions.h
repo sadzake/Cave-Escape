5bb3e648dd862f07f310cb457ac6d2175bc71847
Danin Sadžak <daninsadzak@gmail.com>
2023-02-03 21:35:44 +0100

    Edited instructions.h

--- Headers/Instructions.h
+++ Headers/Instructions.h
@@ -3,5 +3,10 @@
 
 void Instructions() {
     system("CLS");
-    std::cout << "These are the instructions!\n";
+    std::cout << "Welcome to Cave Escape!\n";
+    std::cout << "In Cave Escape, you have to mine, mine, and mine!\n";
+    std::cout << "This is a text-based game, meaning you'll have to type a lot.\n";
+    std::cout << "There are a lot of ways you can type, you can do all lower case (for example, 'instructions') and all uppercase\n";
+    std::cout << "You can as well do one word input (for example, instead of typing 'new game' you can just type 'game')\n";
+    std::cout << "To continue, open the game again. Good luck on your mining journey!\n";
 }
