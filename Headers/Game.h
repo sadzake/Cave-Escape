075a6f6299cc549f3c096eaec3f485215b1787e7
Danin Sadžak <daninsadzak@gmail.com>
2023-02-03 21:43:46 +0100

    Added loading screen

--- Headers/Game.h
+++ Headers/Game.h
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <conio.h>
+#include <windows.h>
 
 void Game() {
     system("CLS");
-    std::cout << "New Game!\n";
+    for(int i = 1; i <= 2; i++) {
+        std::cout << "Loading.";
+        Sleep(1000);
+        system("CLS");
+        std::cout << "Loading..";
+        Sleep(1000);
+        system("CLS");
+        std::cout << "Loading...";
+        Sleep(1000);
+        system("CLS");
+    }
+    std::cout << "DONE\n";
+    Sleep(2000);
+    system("CLS");
+
+    
 }
