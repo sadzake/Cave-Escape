61437812efa228d53e825014d795813c6f27a427
Danin Sadžak <daninsadzak@gmail.com>
2023-02-05 12:57:59 +0100

    Added more to the intro cutscene #2

--- Headers/IntroCutscene.h
+++ Headers/IntroCutscene.h
@@ -18,6 +18,10 @@ void IntroCutscene() {
     SetConsoleTextAttribute(h, 4);
     displayText("'You want a break from life? To enjoy yourself? Well we here at Hotel Ofrygo have just the right destination for you! The island Ofrygo, located in the middle of the Bay of Bengal, is a nice remote island with amazing tourist destinations, such as 'the Statue of King Rajendra', the first king of Ofrygo, who defeated the British Empire who tried to invade the country.\n");
     displayText("Participate in scuba diving, jet skiing, and snorkel near the shores of the world's best beaches!\n");
-    displayText("The best part? It's competely free! If you're one of the lucky 1000 people who call this number on the screen! Call right away and get a free one week trip!'\n\n");
+    displayText("The best part? It's competely free! If you're one of the lucky 1000 people who call this number on the screen! Call right away and get a free one week trip!'\n\n\n");
+    SetConsoleTextAttribute(h, 7);
+    displayText("Intrigued, you call the number right away and a person on the other line says:\n");
+    SetConsoleTextAttribute(h, 4);
+    displayText("'Congratulations! You're one of the lucky 1000 people who got a free trip to Ofrygo!'\n");
     SetConsoleTextAttribute(h, 7);
 }
