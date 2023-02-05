77737ab6c3d60830654ac6f0a4cd8d6b57b1d267
Danin Sadžak <daninsadzak@gmail.com>
2023-02-05 12:53:00 +0100

    Added more to the intro cutscene

--- Headers/IntroCutscene.h
+++ Headers/IntroCutscene.h
@@ -14,8 +14,10 @@ void displayText(std::string text) {
 void IntroCutscene() {
     HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
     system("CLS");
-    SetConsoleTextAttribute(h, 4);
     displayText("While you were watching the television, an ad appears on the TV:\n");
-    displayText("'You want a break from life? To enjoy yourself? Well we here at Hotel Ofrygo have just the right destination for you! The island Ofrygo, located in the middle of the Bay of Bengal, is a nice remote island with amazing tourist destinations, such as 'the Statue of King Rajendra', the first king of Ofrygo, who defeated the British Empire who tried to invade the country.'\n");
+    SetConsoleTextAttribute(h, 4);
+    displayText("'You want a break from life? To enjoy yourself? Well we here at Hotel Ofrygo have just the right destination for you! The island Ofrygo, located in the middle of the Bay of Bengal, is a nice remote island with amazing tourist destinations, such as 'the Statue of King Rajendra', the first king of Ofrygo, who defeated the British Empire who tried to invade the country.\n");
+    displayText("Participate in scuba diving, jet skiing, and snorkel near the shores of the world's best beaches!\n");
+    displayText("The best part? It's competely free! If you're one of the lucky 1000 people who call this number on the screen! Call right away and get a free one week trip!'\n\n");
     SetConsoleTextAttribute(h, 7);
 }
